a0950e5ee76dc8606dad68df1bb168e21287c014
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-09-06 15:11:51 +0800

    Remove auto-closing of dialogs when form

--- src/scripts/logic/form.js
+++ src/scripts/logic/form.js
@@ -1,9 +1,6 @@
-import { closeDialogs } from "../dom/helpers";
-
 export const handleSubmit = function(fn, ...forms) {
     forms.forEach((form) => {
         form.addEventListener("submit", (event) => { event.preventDefault() });
         form.addEventListener("submit", fn);
-        form.addEventListener("submit", closeDialogs);
     });
 }
