c6c3796453dbf9a955ef33370f87ef2a63bee67d
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-09-06 15:00:45 +0800

    Stop form event default before fn exec

--- src/scripts/logic/form.js
+++ src/scripts/logic/form.js
@@ -2,6 +2,7 @@ import { closeDialogs } from "../dom/helpers";
 
 export const handleSubmit = function(fn, ...forms) {
     forms.forEach((form) => {
+        form.addEventListener("submit", (event) => { event.preventDefault() });
         form.addEventListener("submit", fn);
         form.addEventListener("submit", closeDialogs);
     });
